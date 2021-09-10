a8dcd92e82de72a5f9b0b6faf0dfb35c8bed4262
Greg Nishihara <gnishihara@gmail.com>
2021-09-10 15:56:17 +0900

    Add plot

--- course_A_day_01.R
+++ course_A_day_01.R
@@ -254,6 +254,11 @@ read_rds("./winddata_summary.rds")
 
 ################################################################################
 # tibble()
+# HOBO H21-002 Microstation Data @ ECSER
+URL1 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR0FUr9fZ8SbFw3UGS6lulZqbzqW34jtlRj5VmKN8S8QcS4vjYmRafC7v6fwoNljMUlJVTlYRkbrui5/pub?output=csv"
+URL2 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTt_rEpmL7eWJtOc5MprXeihH4LTXkE9CyoLyPext6j3_9wbYAQAXQlEiSOs_Hse0hGLH6-zb6NJVKu/pub?output=csv"
+URL3 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSjodK7zDfhObB8OXSgfq0ZMJQh2d1Q__TFoSJ-6pPnsz50QE34xdJuJ5HQzR5RvyprB2GvsQYPw6Q4/pub?output=csv"
+URL4 = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRn0FUJhsKmyjTNPqahTZnhYvXP-ox6_ze1rwmMCFqpil3vyxsmfBgR-3NyGwfriH8z7xh4v38JemnU/pub?output=csv"
 
 dall = tibble(url = c(URL1, URL2, URL3, URL4)) |> 
   mutate(data = map(url, read_csv, skip = 1))
