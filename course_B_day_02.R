52197769f88b5da4e3b3d6693a1e47321fd61714
Greg Nishihara <gnishihara@gmail.com>
2021-09-13 09:01:53 +0900

    一日目の続きついか

--- course_B_day_02.R
+++ course_B_day_02.R
@@ -11,13 +11,150 @@ library(statmod)
 library(showtext)
 library(ggpubr)
 library(lemon)
+# library(car) # leveneTest() はここにある
+library(emmeans) # 多重比較用パッケージ
+
 
 # ggplot の設定 ################################################################
 font_add_google("Noto Sans", family = "notosans")
 # font_add_google("Noto Serif", family = "notoserif")
 # font_add_google("Noto Sans JP", family = "notosanscjk")
 
-theme_pubr(base_size = 10, base_family = "notosans") |> theme_set()
+# theme_pubr(base_size = 10, base_family = "notosans") |> theme_set()
+theme_pubr(base_size = 10) |> theme_set()
+# Multiple comparisons #########################################################
+
+
+################################################################################
+# 気温のデータを解析してみよう
+# ここでは一元配置分散分析と一元配置共分散分析をします。
+################################################################################
+# データの構造が複雑なので、処理に手間がかかります。
+URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRH8_QwdlSReHgksJaWeRgHJ6J5ELx_7zyFRN7ZVdUHl87vkbZiV9bN42Mf3do8InyTufpQAWF1rKJC/pub?output=csv"
+z = read_lines(URL, skip = 2, n_max = 3)
+z |> as_tibble_col() |> 
+  separate(value, into = str_glue("x{1:75}"), sep = ",") |> 
+  as.matrix() |> t()
+
+x = c("temperature", "high-temperature", "rainfall", "max-rainfall")
+y = c("nagasaki", "tokyo", "naha")
+z = str_glue("{rep(y, each = 4)}_{rep(x, 3)}")
+
+cnames = c("ym", z)
+ctypes = c("cd", rep("-", 4), "d", rep("-", 4), "d", rep("-", 6), "d", rep("-", 6),
+           "d", rep("-", 4), "d", rep("-", 4), "d", rep("-", 6), "d", rep("-", 6),
+           "d", rep("-", 4), "d", rep("-", 4), "d", rep("-", 6), "d", rep("-", 6)) |> 
+  paste(collapse = "")
+
+weather = read_csv(URL, 
+                   col_names = cnames, 
+                   col_types = ctypes, 
+                   skip = 6)
+
+weather = weather |> 
+  pivot_longer(!ym, names_to = c("location", "measurement"),
+               names_pattern = "(.*)_(.*)") |> 
+  separate(ym, c("year", "month"), convert = T)
+################################################################################
+# 気温のデータを抽出してから解析します。
+# 
+temperature = weather |> 
+  filter(str_detect(measurement, "^temperature$")) |> 
+  drop_na()
+
+temperature |> group_by(location) |> 
+  summarise(across(value, list(mean = mean, sd = sd)))
+
+emmeans(m1, specs = pairwise ~ location, adjust = "tukey")
+emtrends(m1, specs = pairwise ~ location, var = "year", adjust = "tukey")
+emmip(m1, ~location, CIs = TRUE)
+emmip(m1, location~year, cov.reduce = range)
+
+################################################################################
+
+
+library(nlme)
+temperature = temperature |> drop_na() |> 
+  mutate(location = factor(location))
+
+m1 = gls(value ~ year2 * location, data = temperature,
+         weights = varIdent(form=~1|location))
+
+summary(m1) 
+
+# Variance for each location
+sigma = summary(m1)$sigma
+
+tokyoS = intervals(m1, which = "var-cov") |> pluck("sigma") |> as_tibble_row() |> 
+  mutate(location = "tokyo")
+S = intervals(m1, which = "var-cov") |> pluck("varStruct") |> 
+  tidyr::as_tibble(rownames = "location")
+
+bind_rows(tokyoS,S) |> 
+  mutate(sigma = c(1,sigma, sigma)) |> 
+  mutate(across(c(lower, `est.`, upper),
+                ~.*sigma))
+
+plot(m1)
+
+temperature = temperature |> mutate(resid = residuals(m1)) |> 
+  mutate(pred = predict(m1))
+
+temperature |> 
+  group_nest(location) |> 
+  mutate(shapiro = map(data, \(x){
+    shapiro.test(x$resid)
+  })) |> 
+  mutate(tidy = map(shapiro, tidy)) |> 
+  unnest(tidy)
+
+bartlett.test(resid ~ location, data = temperature)
+fligner.test(resid ~ location, data = temperature)
+
+ggplot(temperature) + geom_boxplot(aes(x = location, y = resid))
+
+ggplot(temperature) + 
+  geom_point(aes(x = pred, y = resid, color = location)) +
+  geom_smooth(aes(x = pred, y = resid, color = location)) + 
+  facet_wrap(vars(location), nrow = 3)
+
+ggplot(temperature) + 
+  geom_qq(aes(sample = resid, color = location)) +
+  geom_qq_line(aes(sample = resid, color = location)) +
+  facet_wrap(vars(location), ncol = 3)
+
+ggplot(temperature) + 
+  geom_point(aes(x = year, y = value, color = location)) +
+  geom_line(aes(x = year, y = pred, color = location)) +
+  facet_wrap(vars(location), nrow = 3)
+
+
+emmeans(m1, specs = pairwise ~ location, adjust = "tukey")
+
+emtrends(m1, specs = pairwise ~ location, var = "year", adjust = "tukey",
+         mode = "df.error")
+
+emmip(m1, ~location, CIs = TRUE)
+emmip(m1, location~year, cov.reduce = range)
+
+
+
+
+################################################################################
+################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # glm() ########################################################################
 iris2 = iris |> as_tibble()
